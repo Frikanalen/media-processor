f9aec1de9e1b4336ae77680cecbb2c245b62f3d7
Enitoni <enitoni@gmail.com>
2021-12-12 08:52:42 +0100

    add error handling

--- src/modules/core/app.ts
+++ src/modules/core/app.ts
@@ -1,8 +1,10 @@
 import Koa from "koa"
 import bodyParser from "koa-bodyparser"
+import { handleError } from "./middleware/handleError"
 
 const app = new Koa()
 
+app.use(handleError())
 app.use(bodyParser())
 
 export { app }
---
+++ src/modules/core/middleware/handleError.ts
@@ -0,0 +1,40 @@
+import { Middleware } from "koa";
+import { HttpError } from "../classes/HttpError";
+import * as Yup from "yup";
+
+export const handleError = (): Middleware => async (context, next) => {
+  try {
+    return await next();
+  } catch (error) {
+    if (error instanceof HttpError) {
+      const { code, reason, details } = error;
+
+      context.status = code;
+      context.body = {
+        message: reason,
+        details,
+      };
+
+      return;
+    }
+
+    if (error instanceof Yup.ValidationError) {
+      context.status = 400;
+      context.body = {
+        message: "Validation Error",
+        details: error.errors,
+      };
+
+      return;
+    }
+
+    context.status = 500;
+    context.body = {
+      message: "Internal Server Error",
+    };
+
+    console.error(error);
+
+    return;
+  }
+};
