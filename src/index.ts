a4dccb64e4835fbf88aa87dc5b583bd29b75f956
Enitoni <enitoni@gmail.com>
2021-12-15 18:48:35 +0100

    remove useless env check

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { app } from "./modules/core/app"
 
-if (typeof process.env.FK_API === "undefined") {
-  throw new Error("FK_API is undefined!")
-}
-
 const port = Number(process.env.PORT) || 8001
 
 async function main() {
