daa3f033bea61bd7ba1aaadaa16779046fd19b93
Enitoni <enitoni@gmail.com>
2021-12-16 16:10:44 +0100

    use videoRouter

--- src/modules/core/router.ts
+++ src/modules/core/router.ts
@@ -1,8 +1,10 @@
 import Router from "@koa/router"
 import { ingestRouter } from "../ingest/router"
+import { videoRouter } from "../video/router"
 
 const router = new Router()
 
 router.use(ingestRouter.middleware())
+router.use(videoRouter.middleware())
 
 export { router }
