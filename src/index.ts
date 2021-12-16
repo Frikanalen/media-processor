d8b1d53faba05b4752669d4cdf4060099ed7df19
Enitoni <enitoni@gmail.com>
2021-12-16 17:33:14 +0100

    connect to redis client

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import "dotenv/config"
 import { app } from "./modules/core/app"
+import { redis } from "./modules/redis/redis"
 
 const port = Number(process.env.PORT) || 8001
 
 async function main() {
+  await redis.connect()
   app.listen(port, () => console.info(`App listening on port ${port}`))
 }
 
