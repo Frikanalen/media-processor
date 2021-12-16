6e072d3da7c5d36de4b9ec5d460c061d13b3b75d
Enitoni <enitoni@gmail.com>
2021-12-16 15:33:21 +0100

    add secret header to api instance

--- src/modules/core/api.ts
+++ src/modules/core/api.ts
@@ -1,8 +1,11 @@
 import axios from "axios"
-import { API_BASE_URL } from "./constants"
+import { API_BASE_URL, SECRET_KEY, SECRET_KEY_HEADER } from "./constants"
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
   xsrfCookieName: "fk:csrf",
   xsrfHeaderName: "X-CSRF-Token",
+  headers: {
+    [SECRET_KEY_HEADER]: SECRET_KEY!,
+  },
 })
