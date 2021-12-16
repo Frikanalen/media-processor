d676332fff0755470040868fbde0d736d60b7950
Enitoni <enitoni@gmail.com>
2021-12-16 17:06:54 +0100

    add missing router in app

--- src/modules/core/app.ts
+++ src/modules/core/app.ts
@@ -2,11 +2,13 @@ import Koa from "koa"
 import bodyParser from "koa-bodyparser"
 import { handleError } from "./middleware/handleError"
 import { logRequest } from "./middleware/logRequest"
+import { router } from "./router"
 
 const app = new Koa()
 
 app.use(logRequest())
 app.use(handleError())
 app.use(bodyParser())
+app.use(router.middleware())
 
 export { app }
