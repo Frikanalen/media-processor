f31420ce55a0f47cc49bfab96070590802aa6727
Enitoni <enitoni@gmail.com>
2021-12-16 17:32:59 +0100

    Fix processor path

--- src/modules/video/queue.ts
+++ src/modules/video/queue.ts
@@ -1,8 +1,9 @@
 import Queue from "bull"
+import { join } from "path"
 import { VideoJobData } from "./types"
 
 const queue = new Queue<VideoJobData>("video-processing")
 
-queue.process(5, "../processor.ts")
+queue.process(5, join(__dirname, "./processor.js"))
 
 export { queue as videoQueue }
