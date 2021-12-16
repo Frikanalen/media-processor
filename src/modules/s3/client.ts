9b6f1b384984a161a8c552ee49469e45cf0d30eb
Enitoni <enitoni@gmail.com>
2021-12-16 18:43:15 +0100

    Fix missing s3 credentials

--- src/modules/s3/client.ts
+++ src/modules/s3/client.ts
@@ -1,3 +1,10 @@
 import { S3 } from "@aws-sdk/client-s3"
+import { AWS_ENDPOINT } from "./constants"
+const AWS = require("aws-sdk")
 
-export const s3Client = new S3({})
+const credentials = new AWS.EnvironmentCredentials("AWS")
+
+export const s3Client = new S3({
+  endpoint: AWS_ENDPOINT,
+  credentials,
+})
