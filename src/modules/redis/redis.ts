a17fd052796f2c85f3e8c428680b56f05e936b00
Enitoni <enitoni@gmail.com>
2021-12-28 17:49:59 +0100

    add REDIS_URL env variable for configuring redis client

--- src/modules/redis/redis.ts
+++ src/modules/redis/redis.ts
@@ -1,3 +1,11 @@
 import { createClient } from "redis"
 
-export const redis = createClient()
+export const REDIS_URL = process.env.REDIS_URL!
+
+if (!REDIS_URL) {
+  throw new Error("REDIS_URL is not set!")
+}
+
+export const redis = createClient({
+  url: REDIS_URL,
+})
--- src/modules/video/queue.ts
+++ src/modules/video/queue.ts
@@ -1,8 +1,9 @@
 import Queue from "bull"
 import { join } from "path"
+import { REDIS_URL } from "../redis/redis"
 import { VideoJobData } from "./types"
 
-const queue = new Queue<VideoJobData>("video-processing")
+const queue = new Queue<VideoJobData>("video-processing", REDIS_URL)
 
 queue.process(5, join(__dirname, "./processor.js"))
 queue.on("failed", console.log)
