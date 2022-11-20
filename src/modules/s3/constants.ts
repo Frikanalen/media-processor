119d06a5d670b4e055e59ef21fe193a549306ca7
Tore Sinding Bekkedal <toresbe@protonmail.com>
2022-11-20 18:08:33 +0100

    Adapt to rook-ceph buckets

--- src/modules/s3/constants.ts
+++ src/modules/s3/constants.ts
@@ -1,7 +1,7 @@
-export const AWS_ENDPOINT = process.env.AWS_ENDPOINT
+export const AWS_ENDPOINT = `http://${process.env.BUCKET_HOST}:${process.env.BUCKET_PORT}`
 export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
 export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID
-export const AWS_REGION = process.env.AWS_REGION
+export const AWS_REGION = process.env.BUCKET_REGION
 
 if (
   [AWS_ENDPOINT, AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID, AWS_REGION].some(
