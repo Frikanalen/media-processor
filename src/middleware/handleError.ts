a86ba31a45b83bd993d37d7d747138a12e6758d9
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-06-25 22:02:46 +0300

    Add debug statement to handleError.ts

--- src/middleware/handleError.ts
+++ src/middleware/handleError.ts
@@ -1,10 +1,12 @@
 import type { Middleware } from "koa"
 import * as Yup from "yup"
+import { log } from "../log"
 
 export const handleError: Middleware = async (context, next) => {
   try {
     return await next()
   } catch (error: any) {
+    log.error(error)
     if (error instanceof Yup.ValidationError)
       context.throw(400, "validation_error", { details: error.errors })
   }
