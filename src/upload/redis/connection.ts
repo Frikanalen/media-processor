1815d037d4eb024a72fa50e530ae181473f8889a
Tore Sinding Bekkedal <toresbe@gmail.com>
2023-06-30 19:27:56 +0200

    Use kubernetes service URLs for Redis if set

--- src/upload/redis/connection.ts
+++ src/upload/redis/connection.ts
@@ -1,9 +1,20 @@
 import { createClient } from "redis"
 
-export const url = `${process.env["REDIS_URL"]}`
+const getRedisURL = () => {
+  const { REDIS_SERVICE_HOST, REDIS_SERVICE_PORT, REDIS_URL } = process.env
 
-if (!url.length) throw new Error("REDIS_URL is not set!")
+  if (REDIS_SERVICE_HOST?.length && REDIS_SERVICE_PORT?.length)
+    return `redis://${REDIS_SERVICE_HOST}:${REDIS_SERVICE_PORT}`
+
+  if (!REDIS_URL?.length) {
+    throw new Error("REDIS_SERVICE_HOST && REDIS_SERVICE_PORT or REDIS_URL is not set!")
+  }
+
+  return REDIS_URL
+}
+
+export const REDIS_URL = getRedisURL()
 
 export const connection = createClient({
-  url,
+  url: REDIS_URL,
 })
