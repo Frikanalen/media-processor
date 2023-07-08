473d79698c741e81830466b52cb86c6ae489328b
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-07-08 13:58:53 +0200

    Increase maximum size from 500 MB to 50 GB

--- src/upload/router.ts
+++ src/upload/router.ts
@@ -9,7 +9,7 @@ import type { Middleware } from "koa"
 
 export const TUS_RESUMABLE = "1.0.0"
 export const TUS_EXTENSIONS = ["creation", "expiration"]
-export const TUS_MAX_SIZE = 500 * 1000 * 1000 // 500 mb
+export const TUS_MAX_SIZE = 50 * 1024 * 1024 * 1024 // 50 GB
 
 export const setTusHeaders: Middleware = async (ctx, next) => {
   ctx.set("Tus-Extension", TUS_EXTENSIONS.join(","))
